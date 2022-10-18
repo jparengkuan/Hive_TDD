30364d75bbaeb9b8726461b0f9ab6073d949473f
Jim <jim.parengkuan@gmail.com>
2022-10-18 13:37:30 +0200

    Deck klasse aangemaakt en een nieuwe test opgezet voor de deck klasse

---
+++ src/main/java/nl/hanze/hive/Deck.java
@@ -0,0 +1,32 @@
+package nl.hanze.hive;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import nl.hanze.hive.Hive.Player;
+
+/**
+ * This class represents a players deck
+ */
+
+public class Deck {
+
+    // Hashmap to keep track of the players titles
+    private HashMap<Hive.Player, ArrayList<Hive.Tile>> decks;
+
+
+    /**
+     * Constructor
+     */
+    public Deck() {
+        // Create the hashmap
+        this.decks = new HashMap<>();
+    }
+
+    public void createDeck(Player, Player) {
+
+
+    }
+
+
+
+}
