d0dcdb5f1f4fdf410cf4144ce40b05c2a4794905
Jim <jim.parengkuan@gmail.com>
2022-10-20 15:35:09 +0200

    Refactor van de tests

--- src/test/java/nl/hanze/hive/DeckTests.java
+++ src/test/java/nl/hanze/hive/DeckTests.java
@@ -3,7 +3,6 @@ package nl.hanze.hive;
 import org.junit.jupiter.api.Test;
 import nl.hanze.hive.Hive.Player;
 
-import static nl.hanze.hive.Hive.Player.WHITE;
 import static nl.hanze.hive.Hive.Tile.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,31 +16,32 @@ public class DeckTests {
     void whenStartOfGameCheckDeckForPlayerWhite() {
         // Arrange
         Deck deckPlayerWhite = new Deck();
-        Player player = Player.WHITE;
+        Player playerWhite = Player.WHITE;
 
         // Assert
-        assertEquals(1, deckPlayerWhite.countTiles(QUEEN_BEE, player));
-        assertEquals(2, deckPlayerWhite.countTiles(SPIDER, player));
-        assertEquals(2, deckPlayerWhite.countTiles(BEETLE, player));
-        assertEquals(3, deckPlayerWhite.countTiles(GRASSHOPPER, player));
-        assertEquals(3, deckPlayerWhite.countTiles(SOLDIER_ANT, player));
+        assertEquals(1, deckPlayerWhite.countTiles(QUEEN_BEE, playerWhite));
+        assertEquals(2, deckPlayerWhite.countTiles(SPIDER, playerWhite));
+        assertEquals(2, deckPlayerWhite.countTiles(BEETLE, playerWhite));
+        assertEquals(3, deckPlayerWhite.countTiles(GRASSHOPPER, playerWhite));
+        assertEquals(3, deckPlayerWhite.countTiles(SOLDIER_ANT, playerWhite));
     }
 
     /**
      *  (1c) Check if each deck got the right amounts of the tiles from the requirement
      *       For player black
      */
+    @Test
     void whenStartOfGameCheckDeckForPlayerBlack() {
         // Arrange
-        Deck deckPlayerWhite = new Deck();
-        Player player = Player.BLACK;
+        Deck deckPlayerBlack = new Deck();
+        Player playerBlack = Player.BLACK;
 
         // Assert
-        assertEquals(1, deckPlayerWhite.countTiles(QUEEN_BEE, player));
-        assertEquals(2, deckPlayerWhite.countTiles(SPIDER, player));
-        assertEquals(2, deckPlayerWhite.countTiles(BEETLE, player));
-        assertEquals(3, deckPlayerWhite.countTiles(GRASSHOPPER, player));
-        assertEquals(3, deckPlayerWhite.countTiles(SOLDIER_ANT, player));
+        assertEquals(1, deckPlayerBlack.countTiles(QUEEN_BEE, playerBlack));
+        assertEquals(2, deckPlayerBlack.countTiles(SPIDER, playerBlack));
+        assertEquals(2, deckPlayerBlack.countTiles(BEETLE, playerBlack));
+        assertEquals(3, deckPlayerBlack.countTiles(GRASSHOPPER, playerBlack));
+        assertEquals(3, deckPlayerBlack.countTiles(SOLDIER_ANT, playerBlack));
     }
 
 }
