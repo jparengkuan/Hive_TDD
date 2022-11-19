7951621c0b6f67a09eeb047c1bb42d2af7a72f3c
Jim <jim.parengkuan@gmail.com>
2022-11-19 14:22:25 +0100

    whenAnyGivenTileIsMovedThrowIlligalMoveWhenADuringMoveThereIsNoContact() Test

--- src/test/java/nl/hanze/hive/MovementRulesTests.java
+++ src/test/java/nl/hanze/hive/MovementRulesTests.java
@@ -18,4 +18,32 @@ public class MovementRulesTests {
         // check if move has been executed correctly
         assertTrue(board.getCell(0, 0).getTiles().isEmpty() && !board.getCell(1, 0).getTiles().isEmpty());
     }
+
+    @Test
+    void whenAnyGivenTileIsMovedThrowIlligalMoveWhenADuringMoveThereIsNoContact() throws Hive.IllegalMove {
+        Game game = new Game();
+        Board board = game.getBoard();
+        // White
+        game.play(QUEEN_BEE, -1, +1);
+        // Black
+        game.play(QUEEN_BEE, 0, +-3);
+        // White
+        game.play(GRASSHOPPER, -1, 0);
+        // Black
+        game.play(GRASSHOPPER, +1, +-3);
+        // White
+        game.play(BEETLE, 0, -1);
+        // Black
+        game.play(GRASSHOPPER, +2, +-3);
+        // White
+        game.play(BEETLE, +1, -1);
+        // Black
+        game.play(GRASSHOPPER, +3, +-3);
+        // White
+        game.play(GRASSHOPPER, +1, 0);
+        // Black
+        game.move(+3,-3,+2, -2);
+        // White
+        assertThrows(Hive.IllegalMove.class, () -> game.move(-1, +1, 0, +1));
+    }
 }
