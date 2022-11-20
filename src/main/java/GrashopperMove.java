ba0a0a2d3105856bd765a08c66c624c33901fcd2
Jim <jim.parengkuan@gmail.com>
2022-11-20 21:07:33 +0100

    void WhenGrashopperMovesAndEndsInTheSamePositionAsTheBeginningThrowIllegalMove() test

---
+++ src/main/java/GrashopperMove.java
@@ -0,0 +1,35 @@
+import nl.hanze.hive.*;
+
+import java.util.ArrayList;
+
+public class GrashopperMove implements MoveBehavior {
+
+    private Board board;
+
+    public GrashopperMove(Board board) {
+        this.board = board;
+    }
+
+    @Override
+    public void move(int fromQ, int fromR, int toQ, int toR) throws Hive.IllegalMove {
+
+        // Haal de neighbors op van de from coordinaten
+        ArrayList<Cell> neighboursFrom = board.GetNeighboursFromCell(new Cell(fromQ, fromR));
+
+        // Haal de neighbors op van de to coordinaten
+        ArrayList<Cell> neighboursTo = board.GetNeighboursFromCell(new Cell(toQ, toQ));
+
+        neighboursFrom.retainAll(neighboursTo);
+
+        if (neighboursFrom.isEmpty())
+        {
+            throw new Hive.IllegalMove("Beetle tile can only move 1 pos");
+        }
+
+        if(movementHelperMethods.lostContactDuringMove(board, fromQ, fromR, toQ, toR))
+        {
+            throw new Hive.IllegalMove("Beetle tile lost contact during move");
+        }
+
+    }
+}
--- src/test/java/nl/hanze/hive/MovementRulesTests.java
+++ src/test/java/nl/hanze/hive/MovementRulesTests.java
@@ -296,4 +296,21 @@ public class MovementRulesTests {
 
     }
 
+    // (11b)
+    @Test
+    void WhenGrashopperMovesAndEndsInTheSamePositionAsTheBeginningThrowIllegalMove() throws Hive.IllegalMove {
+        Game game = new Game();
+        // White
+        game.play(QUEEN_BEE, -2, -1);
+        // Black
+        game.play(QUEEN_BEE, +1, -1);
+        // White
+        game.play(SOLDIER_ANT, -1, -1);
+        // Black
+        game.play(GRASSHOPPER, +2, -1);
+        // White turn
+        assertThrows(Hive.IllegalMove.class, () -> game.move(-1, -1, -2, -1));
+    }
+
+
 }
