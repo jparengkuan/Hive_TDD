9516aa72c9ca8eefaf40d29aea1e8ec3b926c5e2
FJ Parengkuan <f.j.parengkuan@st.hanze.nl>
2023-01-04 14:24:04 +0100

    whenGameStartThenPlayerHaveACompleteDeck test

--- src/test/java/nl/hanze/hive/HiveGameLogic.java
+++ src/test/java/nl/hanze/hive/HiveGameLogic.java
@@ -1,9 +1,27 @@
 package nl.hanze.hive;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HiveGameLogic {
+
+    //1c. Elke speler heeft aan het begin van het spel de beschikking over één
+    // bijenkoningin, twee spinnen, twee kevers, drie soldatenmieren en drie
+    // sprinkhanen in zijn eigen kleur.
+    @Test
+    void whenGameStartThenPlayerHaveACompleteDeck() {
+        HiveGame hiveGame = new HiveGame();
+        HashMap<Hive.Tile, Integer> playersDeck = hiveGame.getPlayersDeck(Hive.Player.WHITE);
+        assertEquals(Hive.Tile.QUEEN_BEE, 1);
+        assertEquals(Hive.Tile.SPIDER, 2);
+        assertEquals(Hive.Tile.BEETLE, 2);
+        assertEquals(Hive.Tile.SOLDIER_ANT, 3);
+        assertEquals(Hive.Tile.GRASSHOPPER, 3);
+
+    }
 
     // 3. Spelverloop Wit heeft de eerste beurt.
     @Test
