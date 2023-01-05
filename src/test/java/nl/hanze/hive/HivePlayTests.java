76b7ab93f71a1b000cfe057f2b601204c2777663
FJ Parengkuan <f.j.parengkuan@st.hanze.nl>
2023-01-05 13:26:23 +0100

    whenPlayerHasNotPlayedQueenBeeAfterThreeRoundsThrowError test

--- src/test/java/nl/hanze/hive/HivePlayTests.java
+++ src/test/java/nl/hanze/hive/HivePlayTests.java
@@ -14,4 +14,15 @@ public class HivePlayTests {
         hiveGame.play(Hive.Tile.GRASSHOPPER, 1, 0); // Black
         assertThrows(Hive.IllegalMove.class, () -> hiveGame.play(Hive.Tile.QUEEN_BEE, 2, 0));
     }
+
+    //4e Als een speler al drie stenen gespeeld heeft maar zijn bijenkoningin nog niet,
+    // dan moet deze gespeeld worden
+    @Test
+    void whenPlayerHasNotPlayedQueenBeeAfterThreeRoundsThrowError() throws Hive.IllegalMove {
+        HiveGame hiveGame = new HiveGame();
+        hiveGame.playTileFromDeck(Hive.Tile.GRASSHOPPER); // White
+        hiveGame.playTileFromDeck(Hive.Tile.GRASSHOPPER); // White
+        hiveGame.playTileFromDeck(Hive.Tile.SOLDIER_ANT); // White
+        assertThrows(Hive.IllegalMove.class, () -> hiveGame.play(Hive.Tile.QUEEN_BEE, 0, 0));
+    }
 }
