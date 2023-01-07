381647624b259f464f8389cb4fb20e32f9072449
FJ Parengkuan <f.j.parengkuan@st.hanze.nl>
2023-01-07 14:18:24 +0100

    whenPlayerTriesToMoveATileThatsDoesntExistsThrowError test

--- src/test/java/nl/hanze/hive/HiveMovementTests.java
+++ src/test/java/nl/hanze/hive/HiveMovementTests.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class HiveMovementTests {
 
     //5a. Een speler mag alleen zijn eigen eerder gespeelde stenen verplaatsen.
-
     @Test
     void whenPlayerTriesToMoveOpponentsTileThrowError() throws Hive.IllegalMove {
         HiveGame hiveGame = new HiveGame();
@@ -17,4 +16,11 @@ public class HiveMovementTests {
     }
 
     //5a. Een speler kan alleen een steen verplaatsen als deze bestaat op het bord.
+    @Test
+    void whenPlayerTriesToMoveATileThatsDoesntExistsThrowError() throws Hive.IllegalMove {
+        HiveGame hiveGame = new HiveGame();
+        hiveGame.play(Hive.Tile.QUEEN_BEE, 0, 0); //white
+        hiveGame.play(Hive.Tile.GRASSHOPPER, 0, 1); //black;
+        assertThrows(Hive.IllegalMove.class, () -> hiveGame.move(1, 1, 1, 0));
+    }
 }
