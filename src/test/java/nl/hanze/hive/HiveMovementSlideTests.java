116caa13b01965af2b01afdad4c6e69937628f31
FJ Parengkuan <f.j.parengkuan@st.hanze.nl>
2023-01-08 17:43:10 +0100

    whenTileIsNotSlideToAdjacentPositionThrowError test

--- src/test/java/nl/hanze/hive/HiveMovementSlideTests.java
+++ src/test/java/nl/hanze/hive/HiveMovementSlideTests.java
@@ -9,7 +9,28 @@ import static org.mockito.Mockito.*;
 
 public class HiveMovementSlideTests {
 
-    //5c.Tijdens een verschuiving moet de steen continu in contact blijven met
+    //6b. Een verschuiving moet schuivend uitgevoerd kunnen worden. Dit betekent
+    //dat tijdens de verschuiving moet gelden dat de laagste van de twee stapels
+    //die aan het begin- en eindpunt grenzen niet hoger mag zijn dan de hoogste
+    //van de twee stapels op het begin- en eindpunt, waarbij de te verplaatsen
+    //steen niet mee telt
+
+    @Test
+    void whenTileIsNotSlideToAdjacentPositionThrowError() throws Hive.IllegalMove {
+        HiveGame hiveGame = spy(HiveGame.class);
+        when(hiveGame.getPlayersDeck(hiveGame.getCurrenPlayer())).thenReturn(new HashMap<Hive.Tile, Integer>()
+        {{
+            put(Hive.Tile.QUEEN_BEE, 0);
+        }});
+
+        hiveGame.hiveBoard.placeTile(Hive.Tile.SOLDIER_ANT, Hive.Player.WHITE, +1, 0);
+        hiveGame.hiveBoard.placeTile(Hive.Tile.SOLDIER_ANT, Hive.Player.WHITE, -2, 0);
+
+        assertThrows(Hive.IllegalMove.class, () -> hiveGame.move(+1, 0, -1, 0));
+
+    }
+
+    //6c.Tijdens een verschuiving moet de steen continu in contact blijven met
     //minstens één andere steen.
     @Test
     void WhenTileIsLosingContactWithAnotherTileDuringASlideThrowError() throws Hive.IllegalMove {
