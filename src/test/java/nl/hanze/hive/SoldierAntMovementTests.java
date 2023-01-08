74187ef407c541ecae575e433a32328a46815670
FJ Parengkuan <f.j.parengkuan@st.hanze.nl>
2023-01-08 19:02:32 +0100

    WhenPlayerTriesToMoveSoldierAntToStartPositionThrowError test

---
+++ src/test/java/nl/hanze/hive/SoldierAntMovementTests.java
@@ -0,0 +1,28 @@
+package nl.hanze.hive;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+public class SoldierAntMovementTests {
+
+    //8b Een soldatenmier mag zich niet verplaatsen naar het veld waar hij al staat
+    @Test
+    void WhenPlayerTriesToMoveSoldierAntToStartPositionThrowError() throws Hive.IllegalMove {
+        HiveGame hiveGame = spy(HiveGame.class);
+        when(hiveGame.getPlayersDeck(hiveGame.getCurrenPlayer())).thenReturn(new HashMap<Hive.Tile, Integer>()
+        {{
+            put(Hive.Tile.QUEEN_BEE, 0);
+        }});
+
+        hiveGame.hiveBoard.placeTile(Hive.Tile.SOLDIER_ANT, Hive.Player.WHITE, +2, 0);
+        hiveGame.hiveBoard.placeTile(Hive.Tile.SOLDIER_ANT, Hive.Player.WHITE, +1, 0);
+
+        assertThrows(Hive.IllegalMove.class, () -> hiveGame.move(+2, 0, +2, 0));
+    }
+
+}
