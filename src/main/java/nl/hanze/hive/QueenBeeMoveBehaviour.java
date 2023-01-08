f62f903aba12c09f118a49b3cd91296d9117105f
FJ Parengkuan <f.j.parengkuan@st.hanze.nl>
2023-01-08 18:24:13 +0100

    WhenPlayerTriestoMoveQueenBeeToOccupiedPositionThrowError impl

--- src/main/java/nl/hanze/hive/QueenBeeMoveBehaviour.java
+++ src/main/java/nl/hanze/hive/QueenBeeMoveBehaviour.java
@@ -1,12 +1,21 @@
 package nl.hanze.hive;
 
+import java.util.ArrayList;
+
 public class QueenBeeMoveBehaviour extends GenericSlideBehaviour {
     @Override
     public boolean moveIsPossible(HiveBoard hiveBoard, Hexagon toPos, Hexagon fromPos) throws Hive.IllegalMove {
 
-        if(!fromPos.getAllNeighBours().contains(fromPos))
+
+        if(!fromPos.getAllNeighBours().contains(toPos))
         {
             throw new Hive.IllegalMove("De bijenkoningin verplaatst zich alleen door precies een keer te verschuiven");
+        }
+
+        if(hiveBoard.getHiveboard().get(toPos) != null
+            && !hiveBoard.getHiveboard().get(toPos).getTiles().isEmpty())
+        {
+            throw new Hive.IllegalMove("De bijenkoningin kan zich alleen verplaatsen naar een onbezet veld");
         }
 
         if(slideIsPossible(hiveBoard, toPos, fromPos))
