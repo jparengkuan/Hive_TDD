7ba5d2aec8a47d74858dcca16da61b06b63bec15
FJ Parengkuan <f.j.parengkuan@st.hanze.nl>
2023-01-09 19:06:53 +0100

    testSoldierAntMoveBehaviour impl

--- src/main/java/nl/hanze/hive/SoldierAntMoveBehaviour.java
+++ src/main/java/nl/hanze/hive/SoldierAntMoveBehaviour.java
@@ -1,24 +1,43 @@
 package nl.hanze.hive;
 
+import java.util.HashSet;
+
 public class SoldierAntMoveBehaviour extends GenericSlideBehaviour {
     @Override
     public boolean moveIsPossible(HiveBoard hiveBoard, Hexagon toPos, Hexagon fromPos) throws Hive.IllegalMove {
 
-        if(slideIsPossible(hiveBoard, toPos, fromPos))
-        {
-            if (isMovedToStartPosition(toPos, fromPos))
-            {
-                throw new Hive.IllegalMove("Een soldatenmier mag zich niet verplaatsen naar het veld waar hij al staat");
-            }
-
+        if (isMovedToStartPosition(toPos, fromPos)) {
+            throw new Hive.IllegalMove("Een soldatenmier mag zich niet verplaatsen naar het veld waar hij al staat");
         }
-        return false;
+
+        if (findPath(hiveBoard, fromPos, new HashSet<>()).contains(toPos)) {
+            return true;
+        } else {
+            throw new Hive.IllegalMove("De SoldierAnt kan de eindpositie niet bereiken");
+        }
 
     }
 
-    public boolean isMovedToStartPosition(Hexagon toPos, Hexagon fromPos)
-    {
+    private HashSet<Hexagon> findPath(HiveBoard hiveBoard, Hexagon position, HashSet<Hexagon> visitedSet) {
+
+        visitedSet.add(position);
+
+        for (Hexagon neighbour : position.getAllNeighBours()) {
+            if (!visitedSet.contains(neighbour)
+                    //toPos fromPos
+                    && slideIsPossible(hiveBoard, neighbour, position)
+                    && hiveBoard.givenCoordinateHasNeighbours(neighbour.q, neighbour.r)
+                    && !hiveBoard.givenCoordinateHasTiles(neighbour.q, neighbour.r)
+            ) {
+                visitedSet.addAll(findPath(hiveBoard, neighbour, visitedSet));
+            }
+        }
+        return visitedSet;
+    }
+
+    public boolean isMovedToStartPosition(Hexagon toPos, Hexagon fromPos) {
         if (toPos.equals(fromPos)) return true;
         return false;
     }
+
 }
