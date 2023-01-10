fcb754a7a1772c5e17aea63f5d8ee58983103934
FJ Parengkuan <f.j.parengkuan@st.hanze.nl>
2023-01-10 20:49:30 +0100

    WhenPlayerTriesToMoveBeetleToAnOccupiedFieldThrowNowError test

--- src/test/java/nl/hanze/hive/BeetleMovementTests.java
+++ src/test/java/nl/hanze/hive/BeetleMovementTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
@@ -23,6 +24,22 @@ public class BeetleMovementTests {
         hiveGame.hiveBoard.placeTile(Hive.Tile.SOLDIER_ANT, Hive.Player.WHITE, -1, 0);
 
         assertThrows(Hive.IllegalMove.class, () -> hiveGame.move(+2, 0, 0, 0));
+    }
+
+    //7a Een kever kan zich verplaatsen op een bezet veld
+    @Test
+    void WhenPlayerTriesToMoveBeetleToAnOccupiedFieldThrowNowError() {
+        HiveGame hiveGame = spy(HiveGame.class);
+        when(hiveGame.getPlayersDeck(hiveGame.getCurrenPlayer())).thenReturn(new HashMap<Hive.Tile, Integer>()
+        {{
+            put(Hive.Tile.QUEEN_BEE, 0);
+        }});
+
+        hiveGame.hiveBoard.placeTile(Hive.Tile.BEETLE, Hive.Player.WHITE, -1, 0);
+        hiveGame.hiveBoard.placeTile(Hive.Tile.QUEEN_BEE, Hive.Player.WHITE, 0, 0);
+
+        assertDoesNotThrow(() -> hiveGame.move(-1, 0, 0, 0));
+
     }
 
 }
