f3b6c42bc4be43add9c999d7039a4b1c16f3a277
FJ Parengkuan <f.j.parengkuan@st.hanze.nl>
2023-01-10 12:21:40 +0100

    whenPlayerTriesToMoveSpiderOverOccupiedFieldThrowError test

--- src/test/java/nl/hanze/hive/SpiderMovementTests.java
+++ src/test/java/nl/hanze/hive/SpiderMovementTests.java
@@ -56,4 +56,22 @@ public class SpiderMovementTests {
         assertThrows(Hive.IllegalMove.class, () -> hiveGame.move(+2, 0, +2, 0));
     }
 
+    //8c Een spin mag alleen verplaatst worden over en naar lege velden
+    @Test
+    void whenPlayerTriesToMoveSpiderOverOccupiedFieldThrowError() throws Hive.IllegalMove {
+        HiveBoard hiveBoard = spy(HiveBoard.class);
+        when(hiveBoard.getHiveboard()).thenReturn(new HashMap<Hexagon, TileStack>() {{
+            put(new Hexagon(0, -1), new TileStack(new HiveTile(Hive.Player.WHITE, Hive.Tile.QUEEN_BEE)));
+            put(new Hexagon(1, -1), new TileStack(new HiveTile(Hive.Player.WHITE, Hive.Tile.SOLDIER_ANT)));
+            put(new Hexagon(-1, 0), new TileStack(new HiveTile(Hive.Player.WHITE, Hive.Tile.QUEEN_BEE)));
+            put(new Hexagon(1, 0), new TileStack(new HiveTile(Hive.Player.WHITE, Hive.Tile.QUEEN_BEE)));
+            put(new Hexagon(-1, 1), new TileStack(new HiveTile(Hive.Player.WHITE, Hive.Tile.SOLDIER_ANT)));
+            put(new Hexagon(0, 1), new TileStack(new HiveTile(Hive.Player.WHITE, Hive.Tile.QUEEN_BEE)));
+        }});
+
+        SpiderMoveBehaviour spider = new SpiderMoveBehaviour();
+
+        assertThrows(Hive.IllegalMove.class, () -> spider.moveIsPossible(hiveBoard, new Hexagon(0, 0), new Hexagon(-1,-1)));
+    }
+
 }
