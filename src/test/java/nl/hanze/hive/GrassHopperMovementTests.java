018b4ee9e2b481c6acbb636689af9ba3bc54799f
FJ Parengkuan <f.j.parengkuan@st.hanze.nl>
2023-01-11 16:52:15 +0100

    WhenPlayerTriesToMoveGrassHopperToStartPositionThrowError test

---
+++ src/test/java/nl/hanze/hive/GrassHopperMovementTests.java
@@ -0,0 +1,30 @@
+package nl.hanze.hive;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+public class GrassHopperMovementTests {
+
+    //8b Een sprinkhaan mag zich niet verplaatsen naar het veld waar hij al staat.
+    @Test
+    void WhenPlayerTriesToMoveGrassHopperToStartPositionThrowError() {
+        HiveGame hiveGame = spy(HiveGame.class);
+        when(hiveGame.getPlayersDeck(hiveGame.getCurrenPlayer())).thenReturn(new HashMap<Hive.Tile, Integer>()
+        {{
+            put(Hive.Tile.QUEEN_BEE, 0);
+        }});
+
+        hiveGame.hiveBoard.placeTile(Hive.Tile.GRASSHOPPER, Hive.Player.WHITE, +2, 0);
+        hiveGame.hiveBoard.placeTile(Hive.Tile.SOLDIER_ANT, Hive.Player.WHITE, +1, 0);
+
+        assertThrows(Hive.IllegalMove.class, () -> hiveGame.move(+2, 0, +2, 0));
+    }
+
+
+}
