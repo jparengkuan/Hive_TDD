b52d5d297d825af7d51337f7e24beb7cc03f4790
FJ Parengkuan <f.j.parengkuan@st.hanze.nl>
2023-01-11 16:53:54 +0100

    WhenPlayerTriesToMoveGrassHopperToStartPositionThrowError impl

---
+++ src/main/java/nl/hanze/hive/GrassHopperMoveBehaviour.java
@@ -0,0 +1,37 @@
+package nl.hanze.hive;
+
+import nl.hanze.hive.GenericSlideBehaviour;
+import nl.hanze.hive.Hexagon;
+import nl.hanze.hive.Hive;
+import nl.hanze.hive.HiveBoard;
+
+import java.util.HashSet;
+
+public class GrassHopperMoveBehaviour extends GenericSlideBehaviour {
+    @Override
+    public boolean moveIsPossible(HiveBoard hiveBoard, Hexagon toPos, Hexagon fromPos) throws Hive.IllegalMove {
+
+        if (isMovedToStartPosition(toPos, fromPos)) {
+            throw new Hive.IllegalMove("Een sprinkhaan mag zich niet verplaatsen naar het veld waar hij al staat");
+        }
+
+        return true;
+
+    }
+
+    private HashSet<Hexagon> findPath(HiveBoard hiveBoard, Hexagon position, HashSet<Hexagon> visitedSet) {
+
+        return visitedSet;
+    }
+
+    public boolean isMovedToStartPosition(Hexagon toPos, Hexagon fromPos) {
+        if (toPos.equals(fromPos)) return true;
+        return false;
+    }
+
+    @Override
+    public HashSet<Hexagon> getAllEndPositions(HiveBoard hiveBoard, Hexagon fromPos) {
+        return null;
+    }
+
+}
