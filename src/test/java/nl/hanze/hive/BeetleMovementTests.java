0b768c61412ed3d5d9881163e8ef8898038acbc5
FJ Parengkuan <f.j.parengkuan@st.hanze.nl>
2023-01-12 18:36:45 +0100

    WhenPlayerTriesToMoveBeetleMultiplePositionsThrowError refactor

--- src/test/java/nl/hanze/hive/BeetleMovementTests.java
+++ src/test/java/nl/hanze/hive/BeetleMovementTests.java
@@ -13,17 +13,19 @@ public class BeetleMovementTests {
 
     //7a Een kever verplaatst zich door precies één keer te verschuiven.
     @Test
-    void WhenPlayerTriesToMoveBeetleMultiplePositionsThrowError() throws Hive.IllegalMove {
+    void WhenPlayerTriesToMoveBeetleMultiplePositionsThrowError() {
         HiveGame hiveGame = spy(HiveGame.class);
         when(hiveGame.getPlayersDeck(hiveGame.getCurrenPlayer())).thenReturn(new HashMap<Hive.Tile, Integer>()
         {{
             put(Hive.Tile.QUEEN_BEE, 0);
         }});
 
-        hiveGame.hiveBoard.placeTile(Hive.Tile.BEETLE, Hive.Player.WHITE, +2, 0);
-        hiveGame.hiveBoard.placeTile(Hive.Tile.SOLDIER_ANT, Hive.Player.WHITE, -1, 0);
+        hiveGame.hiveBoard.placeTile(Hive.Tile.QUEEN_BEE, Hive.Player.BLACK, 0, 0);
+        hiveGame.hiveBoard.placeTile(Hive.Tile.QUEEN_BEE, Hive.Player.WHITE, 0, 1);
+        hiveGame.hiveBoard.placeTile(Hive.Tile.BEETLE, Hive.Player.WHITE, 0, 1);
+        hiveGame.hiveBoard.placeTile(Hive.Tile.BEETLE, Hive.Player.BLACK, 0, -1);
 
-        assertThrows(Hive.IllegalMove.class, () -> hiveGame.move(+2, 0, 0, 0));
+        assertThrows(Hive.IllegalMove.class, ()  -> hiveGame.move(0, 1, 0, -1));
     }
 
     //7a Een kever kan zich verplaatsen op een bezet veld
